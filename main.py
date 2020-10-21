23e30ad1e3f5620342214998c5ddb7b82fa7900b
Jiamin Zhu <jiamin.zhu@inf.ethz.ch>
2020-10-21 22:59:39 +0200

    get positions

--- main.py
+++ main.py
@@ -8,6 +8,23 @@ def getAccountId():
     id = content.json()[0]["accountId"]
     return id
 
+def getPositions(accountId):
+    pageId = 0
+    positions = []
+
+    while True:
+        url = baseUrl + "/portfolio/" + accountId + "/positions/" + str(pageId)
+        content = requests.get(url, verify=False)
+        for pos in content.json():
+            positions.append(pos)
+        if len(content.json()) == 30:
+            pageId += 1
+        else:
+            break
+
+    return positions
+
 
 accountId = getAccountId()
-print(accountId)
+positions = getPositions(accountId)
+print(positions)

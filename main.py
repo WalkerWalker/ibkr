cf8dc7aea2cc76beabd626219aaae761f86e5b84
Jiamin Zhu <jiamin.zhu@inf.ethz.ch>
2020-10-25 07:38:09 +0100

    refactor position to dict

--- main.py
+++ main.py
@@ -5,42 +5,44 @@ from Position import Position
 from Campaign import Campaign
 
 from pprint import pprint
-from typing import List
+from typing import Dict
 
 import urllib3
 from urllib3.exceptions import InsecureRequestWarning
 urllib3.disable_warnings(category=InsecureRequestWarning)
 
 
-def set_positions_detail(client, positions: List[Position]):
-    conids = [pos.contract.conid for pos in positions]
-    detail_list = client.contracts_definitions(conids)
+def set_positions_detail(client, positions: Dict[int, Position]):
+    conids = positions.keys()
+    detail_list = client.contracts_definitions(list(conids))
     detail_dict = {}
     for detail in detail_list:
         detail_dict[detail['conid']] = detail
-    for pos in positions:
+    for conid in positions.keys():
+        pos = positions[conid]
         contract = pos.contract
-        contract.set_detail(detail_dict[contract.conid])
+        contract.set_detail(detail_dict[conid])
 
 
-def update_positions_mkt_price(client, positions: List[Position]):
-    und_conids = [pos.contract.und_conid for pos in positions]
-    conids = [pos.contract.conid for pos in positions]
-    all_conids = und_conids + conids
+def update_positions_mkt_price(client, positions: Dict[int, Position]):
+    conids = positions.keys()
+    und_conids = [positions[conid].contract.und_conid for conid in conids]
+    all_conids = und_conids + list(conids)
     prices_list = client.market_data(all_conids)
     prices_dict = {}
     for json in prices_list:
         price = float(json['31'].strip("C")) # 31 is the last price in ib api
         prices_dict[json["conid"]] = price
-    for pos in positions:
-        contract = pos.contract
+    for conid in positions.keys():
+        contract = positions[conid].contract
         contract.set_mkt_price(prices_dict[contract.conid])
         contract.set_und_price(prices_dict[contract.und_conid])
 
 
-def get_campaigns(positions: List[Position]):
+def get_campaigns(positions: Dict[int, Position]) -> Dict[int, Campaign]:
     campaigns = {}
-    for pos in positions:
+    for conid in positions.keys():
+        pos = positions[conid]
         und_conid = pos.contract.und_conid
         if und_conid not in campaigns.keys():
             ticker = pos.contract.ticker
@@ -52,21 +54,21 @@ def get_campaigns(positions: List[Position]):
     return campaigns
 
 
-def get_account_id(client:IBClient):
+def get_account_id(client: IBClient) -> str:
     response = client.portfolio_accounts()
     account_id = response[0]["accountId"]
     return account_id
 
 
-def get_positions(client:IBClient, account_id):
+def get_positions(client: IBClient, account_id) -> Dict[int, Position]:
     page_id = 0
-    positions = []
+    positions = {}
     while True:
         response = client.portfolio_account_positions(account_id=account_id, page_id=page_id)
         for pos_json in response:
             pos = Position.parse_json_dict(pos_json)
             if pos is not None:
-                positions.append(pos)
+                positions[pos.contract.conid] = pos
         if len(response) == 30:
             page_id += 1
         else:
@@ -75,7 +77,7 @@ def get_positions(client:IBClient, account_id):
     return positions
 
 
-def write_google_sheet(positions: List[Position]):
+def write_google_sheet(positions: Dict[int, Position]):
     spread_sheet_name = "Options Tracker"
     sheet_name = "Positions"
     scope = ["https://spreadsheets.google.com/feeds",
@@ -90,8 +92,9 @@ def write_google_sheet(positions: List[Position]):
     sheet.clear()
     sheet.append_row(headers)
     rows = []
-    for pos in positions:
-        pos_json_dict = Position.to_json_dict(pos, headers)
+    for conid in positions.keys():
+        pos = positions[conid]
+        pos_json_dict = pos.to_json_dict(headers)
         row = []
         for h in headers:
             value = str(pos_json_dict[h]) if h in pos_json_dict.keys() else ""
